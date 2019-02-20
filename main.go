0f339d6ff4ce3014007e43f3e23eb8c67f33147b
Justin Zollars <j@jz.io>
2019-02-19 20:21:59 -0800

    many changes

--- main.go
+++ main.go
@@ -1,40 +1,52 @@
 package main
 
 import (
-    "github.com/go-chi/chi"
-    "github.com/go-chi/chi/middleware"
-    _ "github.com/go-sql-driver/mysql"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+
+	"diplomacy/driver"
+	gameHandler "diplomacy/handler/http"
 )
 
-var router *chi.Mux
-var db *sql.DB
+func main() {
+	dbName := os.Getenv("DB_NAME")
+	dbPass := os.Getenv("DB_PASS")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
 
-const (
-    dbName = "go-mysql-crud"
-    dbPass = "12345"
-    dbHost = "localhost"
-    dbPort = "33066"
-)
+	fmt.Printf("dbHost%v\n",dbHost)
 
-func routers() *chi.Mux {
-    router.Get("/gamess", AllGames)
-    router.Get("/games/{id}", DetailGame)
-    router.Post("/games", CreateGame)
-    router.Put("/games/{id}", UpdateGame)
-    router.Delete("/games/{id}", DeleteGame)
-    
-    return router
+	connection, err := driver.ConnectSQL(dbHost, dbPort, "root", dbPass, dbName)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+
+	r := chi.NewRouter()
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.Logger)
+
+	h := gameHandler.NewGameHandler(connection)
+	r.Route("/", func(rt chi.Router) {
+		rt.Mount("/games", postRouter(h))
+	})
+
+	fmt.Println("Server listen at :8005")
+	http.ListenAndServe(":8005", r)
 }
 
-func init() { 
-    router = chi.NewRouter() 
-    router.Use(middleware.Recoverer)  
-    
-    dbSource := fmt.Sprintf("root:%s@tcp(%s:%s)/%s?charset=utf8",  dbPass, dbHost, dbPort, dbName)
-    
-    var err error
-    db, err = sql.Open("mysql", dbSource) 
-    
-    catch(err)
-}
+// A completely separate router for posts routes
+func postRouter(h *gameHandler.GameHandler) http.Handler {
+	r := chi.NewRouter()
+	// r.Get("/", pHandler.Fetch)
+	// r.Get("/{id:[0-9]+}", pHandler.GetByID)
+	r.Post("/", h.Create)
+	// r.Put("/{id:[0-9]+}", pHandler.Update)
+	// r.Delete("/{id:[0-9]+}", pHandler.Delete)
 
+	return r
+}
