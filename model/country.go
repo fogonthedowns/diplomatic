6ca8c64a7d9188b78e928d2422443d5757293db4
Justin Zollars <j@jz.io>
2019-02-22 16:49:45 -0800

    add country

---
+++ model/country.go
@@ -0,0 +1,13 @@
+package model
+
+const (
+	ENGLAND        = Country("England")
+	FRANCE         = Country("France")
+	RUSSIA         = Country("Russia")
+	GERMANY        = Country("Germany")
+	AUSTRIA_HUNGRY = Country("Austria-Hungry")
+	ITALY          = Country("Italy")
+	TURKEY         = Country("Turkey")
+)
+
+type Country string
