4c8a84abe7dd100026a1e5cb46c81d1069a74a13
Justin Zollars <142996+fogonthedowns@users.noreply.github.com>
2019-02-23 15:45:15 -0800

    add game user

---
+++ model/game_user.go
@@ -0,0 +1,22 @@
+package model
+
+import "errors"
+
+type GameUser struct {
+	UserId  int     `json:"user_id"`
+	GameId  int     `json:"game_id"`
+	Country Country `json:"country"`
+}
+
+func Validate(gameusers []GameUser, c Country) error {
+	if len(gameusers) == 5 {
+		return errors.New("The Selected Game is Full")
+	}
+
+	for _, game := range gameusers {
+		if game.Country == c {
+			return errors.New("The Country is already Selected")
+		}
+	}
+	return nil
+}
