756969a8b48491febad0be051a877759cf46fc50
Justin Zollars <142996+fogonthedowns@users.noreply.github.com>
2019-02-23 21:03:35 -0800

    a game can be initialized!

--- db/migrations/005_territory.sql
+++ db/migrations/005_territory.sql
@@ -1,5 +1,4 @@
 USE diplomacy;
-DROP TABLE IF EXISTS game_territories;
 DROP TABLE IF EXISTS territory;
 CREATE TABLE territory (
   id int(11) NOT NULL AUTO_INCREMENT,
@@ -8,14 +7,3 @@ CREATE TABLE territory (
   country char(11),
   PRIMARY KEY (id)
 );
-
--- DROP TABLE IF EXISTS game_territories;
--- CREATE TABLE game_territories (
---     game_id int(11) NOT NULL,
---     territory_id int(11) NOT NULL,
---     PRIMARY KEY (game_id, territory_id),
---     CONSTRAINT Constr_Game_Territories_Game_id_fk
---         FOREIGN KEY Game_id_fk (game_id) REFERENCES games (id),
---     CONSTRAINT Constr_Game_Territories_Territory_id_fk
---         FOREIGN KEY Piece_id_fk (territory_id) REFERENCES territory (id)
--- ) CHARACTER SET utf8 COLLATE utf8_general_ci;
