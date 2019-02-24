78439a47b5a0d27347bdc28510c3b6185448326e
Justin Zollars <142996+fogonthedowns@users.noreply.github.com>
2019-02-23 20:20:20 -0800

    this works, but it is messy

--- model/country.go
+++ model/country.go
@@ -1,13 +1,14 @@
 package model
 
 const (
-	ENGLAND        = Country("England")
-	FRANCE         = Country("France")
-	RUSSIA         = Country("Russia")
-	GERMANY        = Country("Germany")
-	AUSTRIA_HUNGRY = Country("Austria-Hungry")
-	ITALY          = Country("Italy")
-	TURKEY         = Country("Turkey")
+	ENGLAND         = Country("England")
+	FRANCE          = Country("France")
+	RUSSIA          = Country("Russia")
+	GERMANY         = Country("Germany")
+	AUSTRIA_HUNGARY = Country("Austria-Hungary")
+	ITALY           = Country("Italy")
+	TURKEY          = Country("Turkey")
+	NONE            = Country("None")
 )
 
 type Country string
