86f937e8d0313484e826ed8b72bd878662b2769e
Justin Zollars <j@jz.io>
2019-02-24 16:49:42 -0800

    update GameInput struct

--- model/game_input.go
+++ model/game_input.go
@@ -5,14 +5,17 @@ import (
 )
 
 type GameInput struct {
-	Id             int64     `json: "id"`
-	Title          string    `json: "title"`
-	StartedAt      time.Time `json: "started_at"`
-	GameYear       time.Time `json: "game_year"`
-	Phase          int       `json: "phase"`
-	PhaseEnd       time.Time `json: "phase_end"`
-	OrdersInterval int       `json: "orders_interval"`
-	GameBoard      GameBoard `json: "game_squares,omitempty"`
-	Country        Country   `json:"country"`
-	UserId         int       `json:"user_id"`
+	Id                int64     `json: "id"`
+	Title             string    `json: "title"`
+	StartedAt         time.Time `json: "started_at"`
+	GameYear          time.Time `json: "game_year"`
+	Phase             int       `json: "phase"`
+	PhaseEnd          time.Time `json: "phase_end"`
+	OrdersInterval    int       `json: "orders_interval"`
+	GameBoard         GameBoard `json: "game_squares,omitempty"`
+	Country           Country   `json:"country"`
+	UserId            int       `json:"user_id"`
+	LocationStart     Territory `json:"location_start"`
+	LocationSubmitted Territory `json:"location_submitted"`
+	PieceId           int       `json:"piece_id"`
 }
