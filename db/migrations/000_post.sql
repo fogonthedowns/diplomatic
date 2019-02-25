b0324688a0ca41649605425c8add343d4806f73b
Justin Zollars <j@jz.io>
2019-02-24 16:21:12 -0800

    add migration helper and move handler

---
+++ db/migrations/000_post.sql
@@ -0,0 +1,3 @@
+USE `diplomacy`;
+INSERT into users (email, user_name, password) VALUES ("tyoo7@yahoo.com", "jaswon wade", "viper");
+INSERT into users (email, user_name, password) VALUES ("jz@jz.com", "justin z", "qwerty");
