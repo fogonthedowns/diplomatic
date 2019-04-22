4d3069cb9707463db5951828cd915b71ace8a58d
Justin Zollars <j@jz.io>
2019-04-21 18:45:45 -0700

    go vet fixes

--- model/game.go
+++ model/game.go
@@ -7,14 +7,14 @@ import (
 )
 
 type Game struct {
-	Id             int64      `json: "id"`
-	Title          string     `json: "title"`
-	StartedAt      *time.Time `json: "started_at"`
-	GameYear       string     `json: "game_year"`
-	Phase          int        `json: "phase"`
-	PhaseEnd       string     `json: "phase_end"`
-	OrdersInterval int        `json: "orders_interval"`
-	GameBoard      GameBoard  `json: "game_squares,omitempty"`
+	Id             int64      `json:"id"`
+	Title          string     `json:"title"`
+	StartedAt      *time.Time `json:"started_at"`
+	GameYear       string     `json:"game_year"`
+	Phase          int        `json:"phase"`
+	PhaseEnd       string     `json:"phase_end"`
+	OrdersInterval int        `json:"orders_interval"`
+	GameBoard      GameBoard  `json:"game_squares,omitempty"`
 }
 
 // DrawGameBoard() Fills in an empty game.GameBoard from data loaded from territory and piece rows in the db
--- model/unit.go
+++ model/unit.go
@@ -1,9 +1,9 @@
 package model
 
 type Unit struct {
-	PieceId     int64    `json:"piece_id",omitempty`
+	PieceId     int64    `json:"piece_id,omitempty"`
 	UnitType    UnitType `json:"unit_type"`
-	Owner       Country  `json:owner"`
+	Owner       Country  `json:"owner"`
 	WillRetreat bool     `json:"will_retreat,omitempty"`
 }
 
