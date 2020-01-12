47375e854337d6d2f4e15d8fd985699f8c783ac9
Justin Zollars <142996+fogonthedowns@users.noreply.github.com>
2020-01-11 17:16:25 -0800

    adds create users table;

--- db/migrations/000_post.sql
+++ db/migrations/000_post.sql
@@ -1,4 +1,14 @@
 USE `diplomacy`;
+
+CREATE TABLE users (
+ `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+ `email` varchar(255) NOT NULL,
+ `user_name`  varchar(255) NOT NULL,
+ `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 INSERT into users (email, user_name, password) VALUES ("one@user.com", "user one", "12345");
 INSERT into users (email, user_name, password) VALUES ("two@user.com", "user two", "12345");
 INSERT into users (email, user_name, password) VALUES ("three@user.com", "user three", "12345");
